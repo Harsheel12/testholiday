35739138a2ef0d2f24684f2a888d7ded18541c8a
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2025-06-11 19:45:26 +1200

    chore: init spring boot backend

---
+++ backend/HomeOrHoliday Backend/src/main/java/com/homeorholiday/backend/HomeOrHolidayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homeorholiday.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeOrHolidayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeOrHolidayBackendApplication.class, args);
+	}
+
+}
